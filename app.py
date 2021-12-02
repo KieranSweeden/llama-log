0705ff69fc977cd561ce9f2d45b40cded82c1031
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-02 16:17:05 +0000

    Get app running using Flask

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
+if os.path.exists("env.py"):
+    import env
+
+# Instance of flask
+app = Flask(__name__)
+
+# Configure app with hidden variables
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# Instance of PyMongo, with flask app inserted as argument
+# so Mongo DB can communicate with flask app
+mongo = PyMongo(app)
+
+@app.route("/")
+def log_in():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
