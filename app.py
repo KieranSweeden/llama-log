9d57ce3a7b7843e1c8cc389431c55ac9de53f94d
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-03 11:37:40 +0000

    Add links to each navbar page in base.html

--- app.py
+++ app.py
@@ -13,17 +13,36 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 # Instance of PyMongo, with flask app inserted as argument
 # so Mongo DB can communicate with flask app
 mongo = PyMongo(app)
+
 
 @app.route("/")
 def log_in():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/feed")
+def feed():
+    return render_template("feed.html")
+
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+
+@app.route("/manage")
+def manage():
+    return render_template("manage.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
