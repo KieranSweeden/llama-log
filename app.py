63088c0feac26acd9c3594ee349b3ec88ae224bf
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-03 11:29:20 +0000

    Add insertion of active nav item classes depending on page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
 if os.path.exists("env.py"):
@@ -20,6 +20,10 @@ mongo = PyMongo(app)
 @app.route("/")
 def log_in():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
