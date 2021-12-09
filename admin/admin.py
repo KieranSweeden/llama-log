5f4499447397c05fe983311abae54454fa404d57
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-09 13:30:02 +0000

    Add redirection for user if a newly created user already exists

--- admin/admin.py
+++ admin/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
 
 
 admin = Blueprint("admin", __name__, static_folder="../static", template_folder="templates")
@@ -10,8 +10,22 @@ def manage():
     return render_template("manage.html")
 
 
-@admin.route("/create_user")
+@admin.route("/create_user", methods=["GET", "POST"])
 def create_user():
+
+    # If an attempt to create a new user has been made
+    if request.method == "POST":
+
+        # Find if user already exists & store it
+        user_exists = True if mongo.db.users.find_one(
+            {"email": request.form.get("email")}) else False
+        
+        # If the user does exist
+        if user_exists:
+            # Redirect the user to the create user page
+            return redirect("create_user.html")
+
+
     return render_template("create_user.html")
 
 @admin.route("/edit_user")
