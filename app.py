72aad7a86ab9d44e0acaa396e272708b33589f52
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-10 11:56:24 +0000

    Add recognise existing user functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import ast
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -34,7 +35,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/log_in", methods=["GET", "POST"])
 def log_in():
-    # If a log in attempt has been made
+    # If an email has been entered
     if request.method == "POST":
 
         # Search db if entered email exists
@@ -44,8 +45,32 @@ def log_in():
         # If email does exist
         if existing_user:
 
-            # Check if the passwords match
-            if check_password_hash(existing_user["password"], request.form.get("password")):
+            return redirect(url_for("password", user_email=existing_user["email"]))
+            
+
+        # If the email entered does not exist
+        # redirect user to log in page & try again
+            return redirect(url_for("log_in"))
+
+    return render_template("index.html")
+
+
+@app.route("/password", methods=["GET", "POST"])
+@app.route("/password/<user_email>", methods=["GET", "POST"])
+def password(user_email):
+
+    # Find the user info via the entered email
+    existing_user = mongo.db.users.find_one(
+        {"email": user_email}) 
+
+    # If a password attempt has been made
+    if request.method == "POST":
+
+        # If reset password is false
+        if existing_user["password_is_reset"] == False:
+
+            # Check if the entered password matches db password
+            if check_password_hash(user_email.password, request.form.get("password")):
 
                 # If so, add user info to current session
                 session["user"] = request.form.get("email")
@@ -57,24 +82,34 @@ def log_in():
                 return redirect(url_for(
                         "user.feed", username=session["user"]))
                 
-            else:
-                # If not, redirect user to log in page & try again
-                return redirect(url_for("log_in"))
-
-        # If the email entered does not exist
-        elif not existing_user:
-
-            # redirect user to log in page & try again
+            # If not, redirect user to log in page & try again
             return redirect(url_for("log_in"))
+        
+        # If a create password attempt has been made
+        # Check both entered passwords are the same
+        if request.form.get("password") == request.form.get("repeat_password"):
 
-    return render_template("index.html")
+            # If so, add the password to the relevant doc in db
+                
+            # If so, add user info to current session
+            session["user"] = request.form.get("email")
+
+            # Make the session permanent for 5 minutes
+            session.permanent = True
+
+            # Redirect logged in user to feed page
+            return redirect(url_for(
+                "user.feed", username=session["user"]))
+
+
+    return render_template("password.html", existing_user=existing_user)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/logout")
+@app.route("/log_out")
 def log_out():
     # Remove current user session data
     session.pop("user")
