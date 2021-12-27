f7b003ae7e0b8a2b4238def4102ecf96827d887a
Kieran Sweeden <kieransweeden@gmail.com>
2021-12-27 13:25:52 +0000

    Add Tippy.js to project with email log-in tooltip shown

---
+++ static/js/tooltips.js
@@ -0,0 +1,17 @@
+// Within this file, is the code relating to Tippy.js
+
+tippy('#email-tooltip', {
+    content: 'Enter your email to sign into Llama Log',
+    trigger: 'click mouseenter',
+    placement: 'right',
+    animation: 'fade',
+    theme: 'llama-log'
+});
+
+tippy('#password-tooltip', {
+    content: 'Enter your password to sign into Llama Log',
+    trigger: 'click mouseenter',
+    placement: 'right',
+    animation: 'fade',
+    theme: 'llama-log'
+});
